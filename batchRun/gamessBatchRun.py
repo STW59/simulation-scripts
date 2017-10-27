52e97e4e638554723be73aaf4da88628fe384217
Stephen E. White <stw59@pitt.edu>
2017-10-27 09:16:17 -0400

    Additional documentation, added check for processed data sets

--- batchRun/gamessBatchRun.py
+++ batchRun/gamessBatchRun.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-# Written by Stephen E. White
-# Last updated : 25OCT2017
+"""
+Written by Stephen E. White
+Last updated : 27OCT2017
 
-# In linux, always run this script as a superuser (su or sudo)
+In linux, always run this script as a superuser (su or sudo).
+This script is designed to run all GAMESS .inp files in the directory from which the script is run.
+It will not double-process data if a .log file for the data set is present.
+"""
 
+# import logger
 import os
 import shutil
+import subprocess
 import time
-from subprocess import call
 
 # Constants that should be edited based on your system
 PATH_TO_GAMESS = '/home/asher/Programs/gamess/'  # Full path to GAMESS folder
@@ -25,6 +30,7 @@ def process_data(input_file, number_of_processors=4):
 
     input_directory = os.getcwd()
 
+    # Copy input file to GAMESS directory
     try:
         shutil.copyfile(os.path.join(input_directory, input_file), os.path.join(PATH_TO_GAMESS, input_file))
     except FileNotFoundError:
@@ -34,7 +40,7 @@ def process_data(input_file, number_of_processors=4):
 
     os.chdir(PATH_TO_GAMESS)
 
-    # CHECKING THAT THERE ARE NO OLD RESIDUAL FILES
+    # Check for and remove all residual files from previous GAMESS runs
     file = ''
     supp_out_files = os.listdir(SUPP_OUTPUT_DIR)
     for file in supp_out_files:
@@ -48,7 +54,7 @@ def process_data(input_file, number_of_processors=4):
             os.remove(os.path.join(TEMP_BINARY_DIR, file))
 
     # Run GAMESS job
-    call(["./rungms", input_file, VERSION, str(number_of_processors), output_name])
+    # subprocess.call(["./rungms", input_file, VERSION, str(number_of_processors), output_name])
 
     # Clean up files from run and copy output to input directory
     try:
@@ -57,7 +63,7 @@ def process_data(input_file, number_of_processors=4):
     except FileNotFoundError:
         pass
     finally:
-        os.chdir(input_directory)
+        os.chdir(input_directory)  # Return to input directory for next job
 
     end_time = time.clock()
 
@@ -66,14 +72,27 @@ def process_data(input_file, number_of_processors=4):
 
 
 def main():
+    # TODO: Set up logger
+
+    # Read all files in working directory
     data_sets = []
+    processed_data_sets = []
     input_dir_list = os.listdir(os.getcwd())
+
+    # Put GAMESS input and log files into data structures for processing
     for file in input_dir_list:
         if file.endswith('.inp'):
             data_sets.append(file)
+        elif file.endswith('.log'):
+            processed_data_sets.append(file)
 
+    # Check to see if file was processed (check .inp against .log)
+    for data_set in data_sets:
+        if (data_set.split('.inp')[0] + '.log') in processed_data_sets:
+            data_sets.remove(data_set)
     data_sets.sort()
 
+    # Run each unprocessed input file
     for input_file in data_sets:
         process_data(input_file)
 
